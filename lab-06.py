edc78fcdf6d001e3d0fefb0edd9b9429d8d2fdb4
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-11-30 16:27:01 +0900

    Update lab-06.py

--- lab-06.py
+++ lab-06.py
@@ -14,16 +14,23 @@ def filter_func(src, dst, filter):
             for k1 in range(-1, 2):
                 for k2 in range(-1, 2):
                     sum += filter[k1 + 1][k2 + 1] * src[i + k1][j + k2]
-            dst[i][j] = max(0, dst[i][j] - sum)
+            dst[i][j] = sum
     print('done.')
 
-# filter = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]], dtype=np.float32)
-filter = np.array([[-1, 0, 1],
-                   [-1, 0, 1],
-                   [-1, 0, 1]], dtype=np.float32)
+filter = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]], dtype=np.float64)
+# filter = np.array([[-1, 0, 0],
+#                    [0, 1, 0],
+#                    [0, 0, 0]], dtype=np.float32)
+
+# filter = np.array([[1, 1, 1],
+#                    [1, -8, 1],
+#                    [1, 1, 1]], dtype=np.float32)
+
 
 img = cv2.imread('image/3.jpg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+img_gray = np.float64(img_gray)
 
 img_filter = img_gray.copy()
 filter_func(img_gray, img_filter, filter)
