ab13986445f04d16062d0d37fa67248d660bd055
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-12-28 10:58:25 +0900

    Update lab-05.py

--- lab-05.py
+++ lab-05.py
@@ -15,28 +15,40 @@ for i in range(img_gray.shape[0]):
     for j in range(img_gray.shape[1] - 1):
         img_dy[i][j] = img_gray[i][j + 1] - img_gray[i][j]
 
-plt.subplot(2, 2, 1)
+img_res = img_gray.copy()
+for i in range(img_gray.shape[0]):
+    for j in range(img_gray.shape[1]):
+        img_res[i][j] = max(0, min(img_res[i][j] - (img_dx[i][j] + img_dy[i][j]), 255))
+
+plt.subplot(3, 2, 1)
 plt.imshow(img, cmap='gray')
 plt.title('Original')
 plt.xticks([])
 plt.yticks([])
 
-plt.subplot(2, 2, 2)
+plt.subplot(3, 2, 2)
 plt.imshow(img_gray, cmap='gray')
 plt.title('Gray')
 plt.xticks([])
 plt.yticks([])
 
-plt.subplot(2, 2, 3)
+plt.subplot(3, 2, 3)
 plt.imshow(img_dx, cmap='gray')
 plt.title('img_dx')
 plt.xticks([])
 plt.yticks([])
 
-plt.subplot(2, 2, 4)
+plt.subplot(3, 2, 4)
 plt.imshow(img_dy, cmap='gray')
 plt.title('img_dy')
 plt.xticks([])
 plt.yticks([])
+
+plt.subplot(3, 2, 5)
+plt.imshow(img_res, cmap='gray')
+plt.title('img_res')
+plt.xticks([])
+plt.yticks([])
+
 
 plt.show()
